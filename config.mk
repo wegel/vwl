04ecf08241ffd514a725b77787a0278e3d72bb3e
Simon Labrecque <simon@wegel.ca>
2026-03-26 19:37:43 -0400

    build against wlroots 0.20

--- config.mk
+++ config.mk
@@ -1,39 +1,21 @@
 _VERSION = 0.0.1-dev
 VERSION  = `git describe --tags --always --dirty 2>/dev/null || echo $(_VERSION)`
 
-PKG_CONFIG = pkg-config
+PKG_CONFIG ?= pkg-config
 
-# paths
 PREFIX = /usr/local
 MANDIR = $(PREFIX)/share/man
 DATADIR = $(PREFIX)/share
 
-WLR_INCS = `$(PKG_CONFIG) --cflags wlroots-0.19`
-WLR_LIBS = `$(PKG_CONFIG) --libs wlroots-0.19`
+WLR_PKG = wlroots-0.20
+WLR_INCS = `$(PKG_CONFIG) --cflags $(WLR_PKG)`
+WLR_LIBS = `$(PKG_CONFIG) --libs $(WLR_PKG)`
 
-# Allow using an alternative wlroots installation
-# This has to have all the includes required by wlroots, e.g:
-# Assuming wlroots git repo is "${PWD}/wlroots" and you only ran "meson setup build && ninja -C build"
-#WLR_INCS = -I/usr/include/pixman-1 -I/usr/include/elogind -I/usr/include/libdrm \
-#	-I$(PWD)/wlroots/include
-# Set -rpath to avoid using the wrong library.
-#WLR_LIBS = -Wl,-rpath,$(PWD)/wlroots/build -L$(PWD)/wlroots/build -lwlroots-0.19
-
-# Assuming you ran "meson setup --prefix ${PWD}/0.19 build && ninja -C build install"
-#WLR_INCS = -I/usr/include/pixman-1 -I/usr/include/elogind -I/usr/include/libdrm \
-#	-I$(PWD)/wlroots/0.19/include/wlroots-0.19
-#WLR_LIBS = -Wl,-rpath,$(PWD)/wlroots/0.19/lib64 -L$(PWD)/wlroots/0.19/lib64 -lwlroots-0.19
-
-XWAYLAND =
-XLIBS =
-# Uncomment to build XWayland support
+# Uncomment to enable XWayland
 #XWAYLAND = -DXWAYLAND
 #XLIBS = xcb xcb-icccm
 
-# dwl itself only uses C99 features, but wlroots' headers use anonymous unions (C11).
-# To avoid warnings about them, we do not use -std=c99 and instead of using the
-# gmake default 'CC=c99', we use cc.
 CC = cc
 
-# Include local configuration overrides (not tracked by git)
+# Local overrides are not tracked by git.
 -include config.local.mk
